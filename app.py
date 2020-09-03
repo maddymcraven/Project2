724c366d9e8718317c0d2b7591946cf74ff491bc
maddymcraven <maddymcraven@gmail.com>
2020-09-02 20:56:40 -0500

    adding final app.py

--- app.py
+++ app.py
@@ -31,34 +31,7 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
-# @app.route("/")
-# def welcome():
-#     """List all available api routes."""
-#     return (
-#         f"Available Routes:<br/>"
-#         f"/api/v1.0/names<br/>"
-#         f"/api/v1.0/passengers"
-#     )
-
-
-# @app.route("/api/v1.0/names")
-# def names():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
-
-#     """Return a list of all passenger names"""
-#     # Query all passengers
-#     results = session.query(Passenger.name).all()
-
-#     session.close()
-
-#     # Convert list of tuples into normal list
-#     all_names = list(np.ravel(results))
-
-#     return jsonify(all_names)
-
-
-@app.route("/")
+@app.route("/all_data")
 def happiness():
     # Create our session (link) from Python to the DB
     session = Session(engine)
@@ -72,16 +45,17 @@ def happiness():
     # Create a dictionary from the row data and append to a list of all_passengers
     all_countries = []
     for Country, Region, Hemisphere, HappinessScore, HDI, GDP_PerCapita, Beer_PerCapita, Spirit_PerCapita, Wine_PerCapita in results:
-        country_dict = {}
-        country_dict["Country"] = Country,
-        country_dict["Region"] = Region,
-        country_dict["Hemisphere"] = Hemisphere,
-        country_dict["HappinessScore"] = HappinessScore,
-        country_dict["HDI"] = HDI,
-        country_dict["GDP_PerCapita"] = GDP_PerCapita,
-        country_dict["Beer_PerCapita"] = Beer_PerCapita,
-        country_dict["Spirit_PerCapita"] = Spirit_PerCapita,
-        country_dict["Wine_PerCapita"] = Wine_PerCapita,
+        country_dict = {
+            "Country": Country,
+            "Region": Region,
+            "Hemisphere": Hemisphere,
+            "HappinessScore": HappinessScore,
+            "HDI": HDI,
+            "GDP_PerCapita": GDP_PerCapita,
+            "Beer_PerCapita": Beer_PerCapita,
+            "Spirit_PerCapita": Spirit_PerCapita,
+            "Wine_PerCapita": Wine_PerCapita
+        }
         all_countries.append(country_dict)
 
     return jsonify(all_countries)
